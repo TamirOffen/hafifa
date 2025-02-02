0fe39456a317ec4c3852f7182db4438b5725717e
Tamir <hofftom342@gmail.com>
2025-02-02 14:11:10 +0200

    Add maybeLogAfterMs in calbacks-logging.js

--- Async_JS/callbacks-logging.js
+++ Async_JS/callbacks-logging.js
@@ -6,13 +6,35 @@ function logAfterMs(message, ms, done) {
     }, ms);
 }
 
-logAfterMs("1", 0, () => {
+function maybeLog(message, errorChance) {
+    const isError = Math.random() < errorChance;
+    if (isError) throw new Error("Something went wrong");
+    console.log(message);
+}
+
+function maybeLogAfterMs(message, ms, done) {
+    // const errorChance = Math.random();
+
+    setTimeout(() => {
+        try {
+            maybeLog(message, 0.5);
+            done();
+        } catch (err) {
+            console.log(err);
+        }
+        
+    }, ms);
+
+}
+
+logAfterMs("1", 0, ()=> {
     logAfterMs("2", 1, ()=>{
-        logAfterMs("3", 10, ()=>{
+        maybeLogAfterMs("3", 10, ()=>{
             logAfterMs("4", 5, ()=>{
                 logAfterMs("5", 0, ()=>{})
             })
         })
-    }) 
+    })
 });
+
 
