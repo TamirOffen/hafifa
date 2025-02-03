dc73d77dcded80e87268fd272ab3236a67eb19d8
Tamir <hofftom342@gmail.com>
2025-02-03 15:12:31 +0200

    Add Callbacks Error Handling in promise-logging.js

--- Async_JS/promise-logging.js
+++ Async_JS/promise-logging.js
@@ -8,16 +8,36 @@ function logAfterMs(message, ms) {
     })
 }
 
+function maybeLog(message, errorChance) {
+    const isError = Math.random() < errorChance;
+    if (isError) throw new Error("Something went wrong");
+    console.log(message);
+}
+
+function maybeLogAfterMs(message, ms) {
+    return new Promise((resolve, reject) => {
+        try {
+            maybeLog(message, 0.5);
+            resolve("finished");
+        } catch (err) {
+            reject(err);
+        }
+    })
+}
+
 logAfterMs("1", 0).then((result) => {
     return logAfterMs("2", 1);
 })
 .then((result) => {
-    return logAfterMs("3", 10);
+    return maybeLogAfterMs("3", 10);
 })
 .then((result) => {
     return logAfterMs("4", 5);
 })
 .then(result => {
     console.log("5");
+})
+.catch(err => {
+    console.log(err);
 })
 
