5204b78ab3d01595587c9ade35cf140902060fc4
Tamir <hofftom342@gmail.com>
2025-02-04 14:35:30 +0200

    Add callbacks pass data in async-await-logging.js

--- Async_JS/async-await-logging.js
+++ Async_JS/async-await-logging.js
@@ -6,11 +6,15 @@ async function logAfterMs(message, ms) {
     console.log(message);
 }
 
-async function maybeLogAfterMs(message, ms, errorChance) {
+async function maybeLogAfterMs(message, ms) {
+    const errorChance = Math.random();
+
     const isError = Math.random() < errorChance;
     if (isError) throw new Error("Something went wrong");
     await setTimeout(ms);
     console.log(message);
+
+    return errorChance;
 }
 
 
@@ -18,9 +22,13 @@ async function run() {
     try {
         await logAfterMs("1", 0);
         await logAfterMs("2", 1);
-        await maybeLogAfterMs("3", 10, 0.5);
-        await logAfterMs("4", 5);
-        await logAfterMs("5", 0);
+        const errorChance = await maybeLogAfterMs("3", 10);
+        if (errorChance > 0.8) {
+            await logAfterMs("4: This is very rare!", 5);
+        } else {
+            await logAfterMs("4: This is very common!", 5);
+        }
+        await logAfterMs("5", errorChance*1_000);
     } catch (err) {
         console.log(err);
     }
